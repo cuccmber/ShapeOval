18f8d7f3596e16ef627eba0f0fc6b82da08a2785
diana <yasuchenya83@gmail.com>
2021-02-26 14:01:04 +0300

    tests added

---
+++ src/test/java/test/yasuchenya/shape/calculation/OvalCalculationTest.java
@@ -0,0 +1,16 @@
+package test.yasuchenya.shape.calculation;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class OvalCalculationTest {
+
+    @Test
+    public void testCalculateArea() {
+    }
+
+    @Test
+    public void testCalculatePerimeter() {
+    }
+}
